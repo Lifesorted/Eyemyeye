77ad1ef1d066c7f9cb578c2969b69a79452f8a27
Lifesorted <mailmeshailendrakumar@gmail.com>
2022-03-30 09:01:01 +0530

    Add home page testcases and add helper class

--- src/main/java/com/Eme/pages/HomePage.java
+++ src/main/java/com/Eme/pages/HomePage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import com.Eme.helper.helper;
+
 public class HomePage {
 
 	private WebDriver driver;
@@ -16,15 +18,48 @@ public class HomePage {
 
 	@FindBy(xpath = "//div[@class=\"logo\"]/child::a")
 	WebElement logo;
+	
+	@FindBy(xpath = "//a[@href=\"/help/\"]")
+	static WebElement helplink;
+	
+	@FindBy(xpath = "//div[@id=\"singnIN\"]")
+	WebElement signin;
+	
+	@FindBy(xpath = "//span[@class=\"cart_icon\"]")
+	WebElement carticon;
+	
+	@FindBy(xpath = "//div[@class=\"new-count\"]")
+	WebElement cartcount;
+	
+	@FindBy(xpath = "//h6[@class=\"font-size-base mb-4 text-center\"]")
+	WebElement cartcounttext;
 
 	// Logo click test
 	public boolean isLogoClickable() {
-		if (logo.isDisplayed()) {
-			logo.click();
-		} else {
-			System.out.println("Logo not present");
+		boolean result= helper.clickElemenetTest(logo);
+		return result;
+	}
+	
+	public boolean helplink() {
+		boolean result= helper.clickElemenetTest(helplink);
+		return result;
+	}
+	
+	public boolean cart() {
+		boolean result= helper.clickElemenetTest(carticon);
+		return result;
+	}
+	
+	public boolean isCartEmpty() {
+		int cartnum=Integer.parseInt(cartcount.getText()); 
+		if (cartnum==0) {
+			cart();
+			cartcounttext.isDisplayed();
+			return true;
+		}else {
+			return false;
 		}
-		return true;
+		
 	}
 
 }
