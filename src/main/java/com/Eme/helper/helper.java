15747220024a3207e529c2366a7e4aa9bc20e6ee
Lifesorted <mailmeshailendrakumar@gmail.com>
2022-04-07 16:43:28 +0530

    Added menu list element test cases

--- src/main/java/com/Eme/helper/helper.java
+++ src/main/java/com/Eme/helper/helper.java
@@ -1,8 +1,15 @@
 package com.Eme.helper;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import com.github.dockerjava.api.model.Driver;
+
 public class helper {
+	
 	
 	public static boolean clickElemenetTest(WebElement element) {
 		if (element.isDisplayed() && element.isEnabled()) {
@@ -12,5 +19,5 @@ public class helper {
 			return false;
 		}
 	}
-
+	
 }
