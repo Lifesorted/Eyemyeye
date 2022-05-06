e686386b95e718b836fa18b2375e2df87675ad8e
Lifesorted <mailmeshailendrakumar@gmail.com>
2022-05-06 23:13:57 +0530

    added data slayer

---
+++ src/main/java/com/Eme/helper/DataSlayer.java
@@ -0,0 +1,43 @@
+package com.Eme.helper;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class DataSlayer {
+
+	static XSSFWorkbook workbook;
+	static XSSFSheet sheet;
+
+	static {
+		File srcFile = new File("./TestData/Datafile.xlsx");
+		FileInputStream fileInputStream;
+		try {
+			fileInputStream = new FileInputStream(srcFile);
+			try {
+				workbook = new XSSFWorkbook(fileInputStream);
+			} catch (IOException e) {
+				System.out.println(e.getMessage());
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+	public static String getData(int sheetnum, int row, int col) {
+		sheet = workbook.getSheetAt(sheetnum);
+		String dataString = sheet.getRow(row).getCell(col).getStringCellValue();
+		return dataString;
+	}
+
+	public static void main(String args[]) {
+		//DataSlayer dSlayer = new DataSlayer();
+		System.out.println(DataSlayer.getData(0, 0, 0));
+	}
+
+}
